245d128cb8e7b89e4a4b0b6204f27cc63b7be91f
Stephanie <slee185@uncc.edu>
2024-11-08 11:14:09 -0500

    got user sign up and log in working

--- VideoGameMeetup/app.js
+++ VideoGameMeetup/app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+const flash = require('connect-flash');
 const eventRoutes = require('./routes/eventRoutes');
 const {fileUpload} = require('./middleware/fileUpload');
 const path = require('path');
-
+const User = require('./models/user');
+const userRoutes = require('./routes/userRoutes');
 
 // Create App
 const app = express();
@@ -17,15 +21,14 @@ let host = 'localhost';
 app.set('view engine', 'ejs');
 const mongoUrl = 'mongodb+srv://JacobMiller:Jm32859299@cluster0.yv4lp.mongodb.net/VideoGameMeetup?retryWrites=true&w=majority&appName=Cluster0';
 
-
+// connect to database
 mongoose.connect(mongoUrl)
 .then(()=>{  
     app.listen(port, host, ()=>{
-    console.log('Server is running on port', port);
-});
+        console.log('Server is running on port', port);
+    });
 })
 .catch(err=>console.log(err.message));
-//console.log(err);
 
 // Mount Middleware
 app.use(express.static('public'));
@@ -34,6 +37,23 @@ app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 
+app.use(session({
+    secret: 'j298tu24p98tup34it',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60*60*1000},
+    store: new MongoStore({mongoUrl})
+}));
+
+app.use(flash());
+
+app.use((req, res, next)=>{
+    console.log(req.session);
+    res.locals.successMessages = req.flash('success');
+    res.locals.errorMessages = req.flash('error');
+    next();
+});
+
 // Set up Routes
 app.get('/', (req, res)=> {
     res.render('index');
@@ -41,6 +61,82 @@ app.get('/', (req, res)=> {
 
 // Mount Routes
 app.use('/events', eventRoutes); 
+app.use('/users', userRoutes);
+
+//get signup form
+app.get('/new', (req, res)=>{
+    res.render('new');
+});
+
+//create a new user
+app.post('/', (req, res, next)=>{
+    let user = new User(req.body);
+    user.save()
+    .then(()=>res.redirect('/login'))
+    .catch(err=>{
+        if(err.name === 'ValidationError') {
+            req.flash('error', err.message);
+            return res.redirect('/new');
+        }
+        if(err.code === 11000) {
+            req.flash('error', 'Email address has been used');
+            return res.redirect('/new');
+        }
+        next(err)
+    });
+});
+
+//get login page
+app.get('/login', (req, res)=>{
+    res.render('login');
+});
+
+//process login request
+app.post('/login', (req, res, next)=>{
+    //authenticate user's login request
+    let email = req.body.email;
+    let password = req.body.password;
+
+    //get user that matches email
+    User.findOne({email: email})
+    .then(user=>{
+        if(user) {
+            //user found in database
+            user.comparePassword(password)
+            .then(result=>{
+                if(result) {
+                    req.session.user = user._id; //store user's id in session
+                    req.flash('success', 'You have successfully logged in!');
+                    res.redirect('/profile');
+                } else {
+                    // console.log('wrong password');
+                    req.flash('error', 'Wrong password!');
+                    res.redirect('users/login');
+                }
+            })
+            .catch(err=>next(err));
+        } else {
+            // console.log('wrong email address');
+            req.flash('error', 'Wrong email address!');
+            res.redirect('users/login');
+        }
+    })
+    .catch(err=>next(err));
+});
+
+//get profile
+app.get('/profile', (req, res, next)=>{
+    let id = req.session.user;
+    User.findById(id)
+    .then(user=>res.render('user/profile', {user}))
+    .catch(err=>next(err));
+});
+
+app.post('/events', fileUpload, (req, res, next) => {
+    let image = "./public/images/" + req.file.filename;
+    res.render('index', {image});
+    console.log('file has been submitted ', req.file, req.body);
+});
 
 app.use((req, res, next) => {
     let err = new Error('Server cannot locate ' + req.url);
@@ -57,16 +153,3 @@ app.use((err, req, res, next)=>{
     res.status(err.status);
     res.render('error', {error: err});
 });
-
-app.post('/events', fileUpload, (req, res, next) => {
-    let image = "./public/images/" + req.file.filename;
-    res.render('index', {image});
-    console.log('file has been submitted ', req.file, req.body);
-});
-
-/*
-// Start the Server
-app.listen(port, host, ()=> {
-    console.log('Server is running on port', port);
-});
-*/
--- VideoGameMeetup/routes/userRoutes.js
+++ VideoGameMeetup/routes/userRoutes.js
@@ -14,17 +14,11 @@ router.get('/profile', controller.profile);
 //POST /users: create a new user
 router.post('/', controller.create);
 
-// POST login submission
-// router.post('/login', controller.processLogin);
-
 // GET login submission
 router.get('/login', controller.login);
 
 // GET logout request
 router.get('/logout', controller.logout);
-
-//GET /users/:id: send details of user identified by id
-// router.get('/:id', controller.show);
 
 //GET /users/:id/edit: send html form for editing an exising user
 router.get('/:id/edit', controller.edit);
